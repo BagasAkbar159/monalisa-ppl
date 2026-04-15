7a75839563cc7d8673a1b689b5fc96ef38988d76
Bagasakbar159 <bagasakbar159@gmail.com>
2026-04-15 19:13:17 +0700

    A

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\Validation\ValidationException;
-use Illuminate\View\View;
+use Illuminate\View\View; 
 
 class RegisteredUserController extends Controller
 {
